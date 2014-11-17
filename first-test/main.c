96ad727b1043ea336d9241f78d1ae0280709b580
Michael Zapf <michael.zapf@fau.de>
2014-11-17 20:14:56 +0100

    first LED blinking test app

---
+++ first-test/main.c
@@ -0,0 +1,15 @@
+#include "board.h" // stm32f3discovery
+#include "hwtimer.h"
+
+int main(void)
+{
+    while(1) {
+        LD3_ON;
+        hwtimer_wait(HWTIMER_TICKS(500 * 1000));
+        LD3_OFF;
+        LD10_ON;
+        hwtimer_wait(HWTIMER_TICKS(500 * 1000));
+        LD10_OFF;
+    }
+    return 0;
+}

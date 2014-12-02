a23f04b558e42fe384e9a606768a90005b2c393a
jschnurrer <github@jschnurrer.de>
2014-12-02 18:17:51 +0100

    f0-board main c1101 Anfang

--- first-test-f0/main.c
+++ first-test-f0/main.c
@@ -2,108 +2,17 @@
 #include "cpu.h"
 #include "periph/spi.h"
 #include "hwtimer.h"
+#include "cc110x_legacy.h"
 
-
-//dies ist eine kleine Testanwendung für meine LPD8896 LED-Leiste.
-// und funktioniert sogar!
-
-
-#define 	TEST_WAIT	10
 
 int main(void)
 {
 
-	//noetig? Nein, LE nicht notwendig fuer SPI-Konfiguration.
-    //GPIOA->MODER &= ~(2 << (2 * 4));           /* set pin to output mode */
-    //GPIOA->MODER |= (1 << (2 * 4));
-    //GPIOA->OTYPER &= ~(1 << 4);                /* set to push-pull configuration */
-    //GPIOA->OSPEEDR |= (3 << (2 * 4));          /* set to high speed */
-    //GPIOA->PUPDR &= ~(3 << (2 * 4));           /* configure push-pull resistors */
-    //GPIOA->PUPDR |= (0b00 << (2 * 4));
-    //GPIOA->ODR &= ~(1 << 4);                   /* set pin to low signal */
 
+	//transceiver_pid = thread_create(transceiver_stack, TRANSCEIVER_STACK_SIZE, PRIORITY_MAIN - 3, CREATE_STACKTEST, run, NULL, "Transceiver");
 
-	if(spi_init_master(SPI_0, SPI_CONF_FIRST_RISING, SPI_SPEED_100KHZ) != 0){
-		while(1) LD3_ON; //nix mehr machen
+	//cc110x_init(transceiver_pid);
 
-	}
-
-	char blubb = 0;
-	char *mirwors = &blubb; //null und NULL wollte er nicht, daher halt umstaendlich
-
-    while(1) {
-		for(uint8_t i = 0; i<30; i++){
-
-			//blau
-			if(spi_transfer_byte(SPI_0, 0x8f, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-
-			hwtimer_wait(HWTIMER_TICKS(TEST_WAIT * 1000));
-
-			//gruen
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x8f, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-
-			hwtimer_wait(HWTIMER_TICKS(TEST_WAIT * 1000));
-		}
-
-		for(uint16_t i=((60+31)/32); i>0; i--) {
-			if(spi_transfer_byte(SPI_0, 0x00, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-		}
-
-
-		for(uint8_t i = 0; i<30; i++){
-
-			//dunkel
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-
-			hwtimer_wait(HWTIMER_TICKS(TEST_WAIT * 1000));
-
-			//rot
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x8f, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-			if(spi_transfer_byte(SPI_0, 0x80, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-
-			hwtimer_wait(HWTIMER_TICKS(TEST_WAIT * 1000));
-		}
-
-
-		for(uint16_t i=((60+31)/32); i>0; i--) {
-			if(spi_transfer_byte(SPI_0, 0x00, mirwors) != 1){
-				while(1) LD3_ON; //nix mehr machen
-			}
-		}
 
 
         //LD3_ON;
@@ -112,6 +21,7 @@ int main(void)
         //LD4_ON;
         //hwtimer_wait(HWTIMER_TICKS(500 * 1000));
         //LD4_OFF;
-    }
+
     return 0;
+
 }

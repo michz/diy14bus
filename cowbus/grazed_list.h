ca65bc047645a574218f2fa43eb6a39ec230c05c
Michael Zapf <michael.zapf@fau.de>
2015-01-09 17:55:03 +0100

    first cowbus header inserted (ideas)

---
+++ cowbus/grazed_list.h
@@ -0,0 +1,45 @@
+/**
+ * @brief   TODO
+ *
+ * TODO
+ *
+ * @author  TODO
+ * @file
+ */
+
+
+#ifndef GRAZED_LIST_H
+#define GRAZED_LIST_H
+
+// TODO definiere Liste der zuletzt empfangenen Pakete
+// - sollte sowas wie FIFO-Liste sein
+// - könnte wie Ringpuffer realisiert sein:
+//   Ein Schreibzeiger merkt sich, wo als nächstes geschrieben wird.
+//   Gelesen werden wenn dann ja eh immer alle im Kreis.
+// - Leere Einträge sollten 0 sein.
+//
+// - grazed_delete_cyclic() wird regelmäßig aufgerufen
+//   und löscht zyklisch die Einträge, damit keiner zu alt ist.
+
+typedef struct grazed_header {
+    // TODO: besteht aus SeqNr und Addr
+} grazed_header;
+
+
+/**
+ * Checks if packet is already on list.
+ * If no, return 0, otherwise return nonzero value.
+ * If table full, oldest entry is overwritten.
+ * If new entry is the one that would be deleted next by grazed_delete_cyclic()
+ * move this pointer one entry ahead.
+ */
+int grazed_add(SeqNr, Addr); //TODO
+
+/**
+ * Clears the next entry.
+ * This is used to assure that no entry is too old.
+ */
+void grazed_delete_cyclic(); // TODO
+
+
+#endif // GRAZED_LIST_H

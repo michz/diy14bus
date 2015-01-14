2b0c2849fb1cdf2e94927b4ec53856cb2eaf6a25
Michael Zapf <michael.zapf@fau.de>
2015-01-14 18:32:26 +0100

    cowpacket.ts further

--- cowbus-config/js/cowpacket.js
+++ cowbus-config/js/cowpacket.js
@@ -1,3 +1,14 @@
+var cowpacket_type;
+(function (cowpacket_type) {
+    cowpacket_type[cowpacket_type["undefined_packet"] = 0] = "undefined_packet";
+    cowpacket_type[cowpacket_type["event_packet"] = 1] = "event_packet";
+    cowpacket_type[cowpacket_type["program"] = 2] = "program";
+    cowpacket_type[cowpacket_type["get_name"] = 3] = "get_name";
+    cowpacket_type[cowpacket_type["get_state"] = 4] = "get_state";
+    cowpacket_type[cowpacket_type["get_config"] = 5] = "get_config";
+    cowpacket_type[cowpacket_type["ping"] = 6] = "ping";
+    cowpacket_type[cowpacket_type["ping_answer"] = 7] = "ping_answer";
+})(cowpacket_type || (cowpacket_type = {}));
 var cowpacket = (function () {
     function cowpacket() {
     }
--- cowbus-config/js/cowpacket.ts
+++ cowbus-config/js/cowpacket.ts
@@ -1,12 +1,22 @@
+enum cowpacket_type {
+    undefined_packet    = 0,
+    event_packet        = 1,
+    program             = 2,
+    get_name            = 3,
+    get_state           = 4,
+    get_config          = 5,
+    ping                = 6,
+    ping_answer         = 7
+}
 class cowpacket {
+    version : number;
     seq_no  : number;
     ttl     : number;
     address : number;
-    type    : number;
+    type    : cowpacket_type;
     is_fragment: boolean;
     payload : string;
     crc     : string;
-    
 }
 
 

ddc9ac8d52d790ee6d5249e86e1cf85ded7d4ed5
Michael Zapf <michael.zapf@fau.de>
2015-01-14 18:12:22 +0100

    added field version to cowpacket

--- cowbus/cowpacket.h
+++ cowbus/cowpacket.h
@@ -26,12 +26,12 @@ typedef enum cowpacket_type {
 } cowpacket_type;
 
 typedef struct cowpacket {
+    unsigned int version            : 3;
     unsigned int seq_no             : 5;
     unsigned int ttl                : 8;
     unsigned int addr               : 11;
     cowpacket_type type             : 4;
     unsigned int is_fragment        : 1;
-    unsigned int reserved           : 3;
     unsigned char payload[PAYLOAD_MAX_LENGTH];
     unsigned char checksum[2];
 } cowpacket;

30e667f4153fb5e969444373def59deff81c0ca6
jschnurrer <github@jschnurrer.de>
2015-01-21 15:03:12 +0100

    led1 fertig

--- cowbus/led.c
+++ cowbus/led.c
@@ -10,15 +10,25 @@
 #include "led.h"
 
 void led_init(void) {
-    //TODO configure GPIOs
+
+    //led1
+    //GPIO_3    LED
+    gpio_init_out(GPIO_3, GPIO_NOPULL); //set as output
+    gpio_clear(GPIO_3); //set to low -> led off
+    
+    //led2: rgb //TODO
+    //GPIO_0    RGB-LED
+    //GPIO_1    RGB-LED
+    //GPIO_2    RGB-LED
+    
 }
 
 void led1_switch_state(bool on) {
-    //TODO GPIO switch on / off
+    gpio_write(GPIO_3, (int)on); //should work, or is "if then else" differenciation for input value on necessary?
 }
 
 inline void led1_on(void) {
-    led1_switch_state(false);
+    led1_switch_state(true);
 }
 
 inline void led1_off(void) {
@@ -27,5 +37,8 @@ inline void led1_off(void) {
 
 void led2_set_color(led_color color) {
     //TODO GPIO switch on / off
+    
+
+    
 }
 
